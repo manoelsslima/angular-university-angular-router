ebbc68c2e7b02d6562e02b06ad9b7875369d6093
Manoel Lima <manoel.lima@manaus.am.gov.br>
2024-07-09 11:18:27 -0400

    Angular Router Module Extra Options - enableTracing and useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,7 +44,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: CustomPreloadingStrategy
+      preloadingStrategy: CustomPreloadingStrategy,
+      enableTracing: false,
+      useHash: true
     })
   ],
   exports: [RouterModule],
